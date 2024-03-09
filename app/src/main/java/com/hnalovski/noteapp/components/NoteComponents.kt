bbaf0c64e32c7f6bfbc60423be34c5854534bda8
Hristijan Nalovski <nalovski.hristijan@protonmail.com>
2024-03-09 15:17:30 +0100

    Created the top app bar and started designing the text fields

---
+++ app/src/main/java/com/hnalovski/noteapp/components/NoteComponents.kt
@@ -0,0 +1,42 @@
+package com.hnalovski.noteapp.components
+
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun NoteInputText(
+    modifier: Modifier = Modifier,
+    text: String,
+    label: String,
+    maxLine: Int = 1,
+    onTextChange: (String) -> Unit,
+    onImeAction: () -> Unit = {}
+) {
+
+    val keyBoardController = LocalSoftwareKeyboardController.current
+    TextField(
+        value = text,
+        onValueChange = onTextChange,
+        colors = TextFieldDefaults.colors(Color.Transparent),
+        modifier = modifier,
+        label = { Text(text = label) },
+        maxLines = maxLine,
+        keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+        keyboardActions = KeyboardActions(onDone = {
+            onImeAction()
+            keyBoardController?.hide()
+        })
+        
+    )
+
+}
